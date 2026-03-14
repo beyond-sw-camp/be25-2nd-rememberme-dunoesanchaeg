8ef5a00a7203305b67300bc1bb2839d313c7d5df
wnstkd704 <wnstkd704@gmail.com>
2026-03-14 09:56:55 +0900

    chore: 패키지, 설정 구성

---
+++ src/main/java/com/rememberme/dunoesanchaeg/DunoesanchaegApplication.java
@@ -0,0 +1,14 @@
+package com.rememberme.dunoesanchaeg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DunoesanchaegApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DunoesanchaegApplication.class, args);
+	}
+
+}
