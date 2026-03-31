cdaefeec5fe10f783d6edcc89ec3b599ef8f2279
wnstkd704 <wnstkd704@gamil.com>
2026-03-31 18:43:05 +0900

    docs: SecurityConfig calendar 경로 추가

--- src/main/java/com/rememberme/dunoesanchaeg/config/SecurityConfig.java
+++ src/main/java/com/rememberme/dunoesanchaeg/config/SecurityConfig.java
@@ -50,6 +50,7 @@ public class SecurityConfig {
                                 "/api/v1/open-questions/**", // 질문
                                 "/api/v1/daily-records/**",  // 기록
                                 "/api/v1/statistics/**",     // 통계
+                                "/api/v1/calendar/**",       // 캘린더
                                 "/v3/api-docs/**",           // Swagger용
                                 "/swagger-ui/**"             // Swagger UI용
                         ).permitAll()
