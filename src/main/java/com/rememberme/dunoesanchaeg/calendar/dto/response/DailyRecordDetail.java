41d5a22c71e782e6a13d7149e3003383032b01b7
wnstkd704 <wnstkd704@gamil.com>
2026-04-02 11:24:37 +0900

    refactor: 필요없는 어노테이션 정리

--- src/main/java/com/rememberme/dunoesanchaeg/calendar/dto/response/DailyRecordDetail.java
+++ src/main/java/com/rememberme/dunoesanchaeg/calendar/dto/response/DailyRecordDetail.java
@@ -5,12 +5,8 @@ import com.rememberme.dunoesanchaeg.memory.domain.enums.Level;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
-@Setter
-@NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class DailyRecordDetail {
