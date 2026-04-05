389b5f75ce1ea12c34181344fd235153ac042594
0lthree <oithreed@gmail.com>
2026-04-05 18:28:29 +0900

    fix: 개방형질문 기능 후속 수정(서버 오류 등 사용자가 명시적으로 이탈 의사를 표현하지 않은 경우에는 재시작 가능)

--- src/main/java/com/rememberme/dunoesanchaeg/memory/service/DailyQuestionLogService.java
+++ src/main/java/com/rememberme/dunoesanchaeg/memory/service/DailyQuestionLogService.java
@@ -10,6 +10,9 @@ public interface DailyQuestionLogService {
 
     boolean existsTodayQuestionLog(Long memberId, LocalDate recordDate, DailyQuestionLogStatus status);
 
+    // 추가
+    DailyQuestionLog getTodayQuestionLog(Long memberId, LocalDate recordDate);
+
     Long createTodayQuestionLog(DailyQuestionLog dailyQuestionLog);
 
     void delete(Long memberId, Long dailyQuestionLogId);
