d26009161a618b6f19220445539e0d7b4b958054
wnstkd704 <wnstkd704@gamil.com>
2026-04-11 23:57:04 +0900

    refactor: 스웨거 고정 날짜에서 모든 날짜를 받을 수 있게 수정

--- src/main/java/com/rememberme/dunoesanchaeg/calendar/dto/request/CalendarSummaryRequest.java
+++ src/main/java/com/rememberme/dunoesanchaeg/calendar/dto/request/CalendarSummaryRequest.java
@@ -17,7 +17,7 @@ public class CalendarSummaryRequest {
 
 	@NotBlank(message = "필수 파라미터입니다. (YYYY-MM-DD 형식으로 입력해주세요.)")
 	@Pattern(
-			regexp = "2026-03-31",
+			regexp = "^\\d{4}-\\d{2}-\\d{2}$",
 			message = "필수 파라미터입니다. (YYYY-MM-DD 형식으로 입력해주세요.)"
 	)
 	private String targetDate;
